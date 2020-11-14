f5b3c6c391888504ec49d15eb06076b994400980
Masha <mashabudryte@gmail.com>
2020-11-14 15:27:16 +0000

    add links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Footer from './pageSections/footer'
 function App() {
   return (
-    <div className="App">
-      <h1>HI!</h1>
-    <p>My name is Masha Budryte and I am a software developer and science journalist. </p>
+    <div className="App"> 
+      <p>My name is Masha Budryte and I am a software developer and science journalist. </p>
+      <Footer/>
     </div>
   );
 }
---
+++ src/pageSections/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div class="footer">
+            <a href="https://github.com/m-budryte" class="fa fa-github"></a>
+            <a href="https://www.linkedin.com/in/marija-budryte/" class="fa fa-linkedin"></a>
+            <a href="https://telegram.me/mashabudryte" class="fa fa-telegram"></a>
+            <a href="mailto:mashabudryte@gmail.com" class="fa fa-envelope"></a>
+        </div>
+        )
+}
+export default Footer;
