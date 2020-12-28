79cbe08f959180976961b97722b8d57eb0b59dbc
Masha Budryte <masha.budryte@thoughtworks.com>
2020-12-28 15:24:37 +0000

    add pictures instead of articles

--- src/websiteSections/ArticlesSection/articlesSection.js
+++ src/websiteSections/ArticlesSection/articlesSection.js
@@ -1,23 +1,52 @@
-import React from 'react';
-import './articlesSection.css'
+import React from "react";
+import "./articlesSection.css";
+import laptop from "./articleImages/laptop.jpg";
+import baton from "./articleImages/baton.png";
+import kartoshka from "./articleImages/kartoshka.jpg";
+
 class ArticlesSection extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>Top articles published to date:</p>
-                <h5>2020</h5>
-                <p><a className="articleLink" href="https://www.thoughtworks.com/insights/blog/its-more-just-pronoun">It's more than just a pronoun </a></p>
-                <p><a className="articleLink" href="https://www.wonderzine.com/wonderzine/health/food/253537-vredny-li-proteinovye-batony">Волшебные палочки: Полезны ли протеиновые
-батончики на самом деле </a></p>
-<p><a className="articleLink" href="https://www.wonderzine.com/wonderzine/health/food/253159-demonizatsiya-kartoshki">Овощ с плохой репутацией: Почему не стоит демонизировать картошку
- </a></p>
- <h5>2019</h5>
- <p><a className="articleLink" href="https://www.wonderzine.com/wonderzine/health/food/244701-soup-myths">Разоблачение супа:
-Почему здоровый рацион возможен и без него
- </a></p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <p>Top articles published to date:</p>
+        <h5>2020</h5>
+        <div className="articleLink">
+          <a href="https://www.thoughtworks.com/insights/blog/its-more-just-pronoun">
+            <img src={laptop} alt="laptop" />
+            <p className="title">It's more than just a pronoun</p>
+            <p className="publisher">ThoughtWorks</p>
+          </a>
+        </div>
+        <div className="articleLink">
+          <a href="https://www.wonderzine.com/wonderzine/health/food/253537-vredny-li-proteinovye-batony">
+            <img src={baton} alt="protein bar" />
+            <p className="title">
+              Волшебные палочки: Полезны ли протеиновые батончики на самом деле
+            </p>
+            <p className="publisher">Wonderzine</p>
+          </a>
+        </div>
+        <div className="articleLink">
+          <a href="https://www.wonderzine.com/wonderzine/health/food/253159-demonizatsiya-kartoshki">
+            <img src={kartoshka} alt="potato" />
+            <p className="title">
+              Овощ с плохой репутацией: Почему не стоит демонизировать картошку
+            </p>
+            <p className="publisher">Wonderzine</p>
+          </a>
+        </div>
+        <h5>2019</h5>
+        <p>
+          <a
+            className="articleLink"
+            href="https://www.wonderzine.com/wonderzine/health/food/244701-soup-myths"
+          >
+            Разоблачение супа: Почему здоровый рацион возможен и без него
+          </a>
+        </p>
+      </div>
+    );
+  }
 }
 
 export default ArticlesSection;
